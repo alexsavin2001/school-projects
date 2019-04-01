11c5c29441841fd495a1793f25cec67b6325e0a4
alexsavin2001 <35452999+alexsavin2001@users.noreply.github.com>
2019-04-01 21:41:34 +0300

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,18 +1,29 @@
 #include<iostream>
 using namespace std;
 int n=5;
+int poz=1;
 int M[100]={35565,533,244,104,87,18};
 
 
-int makeheap(int nume_nod,int heap_size)
+int find_heap(int nume_nod,int heap_size,int value)
 {
-    if (nume_nod<=heap_size)
-    { cout<<M[nume_nod]<<" ";
-       makeheap(nume_nod*2,heap_size);
-       makeheap(nume_nod*2+1,heap_size);}
+    if(M[nume_nod]==value)
+        {cout<<poz; return 1;}
+    else
+    {
+
+        if(nume_nod<=heap_size)
+        {
+            poz=poz*10+1;
+        find_heap(nume_nod*2, heap_size, value);
+        poz=(poz/10)*10;
+        find_heap(nume_nod*2+1, heap_size, value);}
+    }
+    return 0;
 }
 
 int main()
 {
-   makeheap(1,n);
+   cout<<"Some huffman code : "<<endl;
+   find_heap(1,n,87);
 }
